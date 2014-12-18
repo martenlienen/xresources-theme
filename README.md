0bdfc8d253603752428f9cf6cbed63db1cfd701c
Marten Lienen <marten.lienen@gmail.com>
2014-12-18 21:49:03 +0100

    Create the basic package skeleton

---
+++ README.md
@@ -0,0 +1,4 @@
+# .Xresources-theme
+
+This theme reads it's colors from your `~/.Xresources` file, so that you can
+easily keep your emacs theme in sync with your xterm and other X applications.
---
+++ xresources-theme.el
@@ -0,0 +1,31 @@
+;;; xresources-theme.el --- Use your .Xresources as your emacs theme
+
+;; Copyright (C) 2014-2014 Marten Lienen <marten.lienen@gmail.com>
+
+;; Author: Marten Lienen <marten.lienen@gmail.com>
+;; Keywords: xresources, theme
+;; Version: 0.1.0
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation; either version 3, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License along with
+;; GNU Emacs; see the file COPYING.  If not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+;; USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'xresources-theme)
+
+;;; xresources-theme.el ends here
