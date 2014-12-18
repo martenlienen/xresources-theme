21c383322b1d43e111c37d0d13ce6c1fc8f587a0
Marten Lienen <marten.lienen@gmail.com>
2014-12-18 22:15:39 +0100

    Automatically put this file on the theme path

--- xresources-theme.el
+++ xresources-theme.el
@@ -26,6 +26,15 @@
 
 ;;; Code:
 
-(provide 'xresources-theme)
+(deftheme xresources "~/.Xresources as a theme")
+
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
+
+(provide-theme 'xresources)
 
 ;;; xresources-theme.el ends here
